ac2c382259525081b1ad7dbed0deb098add872f2
Carol Willing <carolcode@willingconsulting.com>
2017-09-26 20:28:15 -0700

    update theme

--- doc/conf.py
+++ doc/conf.py
@@ -19,6 +19,7 @@
 #
 import os
 import sys
+
 curdir = os.path.dirname(__file__)
 sys.path.append(os.path.abspath(os.path.join(curdir, 'script')))
 
@@ -97,7 +98,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'jupyter_alabaster_theme'
+html_theme = 'jupyter_alabaster_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
