017758df55c866943597eb3262b3843ef0b8ed0e
Carol Willing <carolcode@willingconsulting.com>
2018-11-15 20:45:43 -0800

    update paths

--- doc/conf.py
+++ doc/conf.py
@@ -10,17 +10,17 @@ import sys
 import alabaster_jupyterhub
 import requests
 
-# Set paths
+# Get the project root dir, which is the parent dir of this
+cwd = os.getcwd()
+project_root = os.path.dirname(cwd)
+
+# Insert the project root dir as the first element in the PYTHONPATH.
+# This lets us ensure that the source package is imported, and that its
+# version is used.
 sys.path.insert(0, os.path.abspath('.'))
-docs = dirname(__file__)
-print(docs)
-root = dirname(docs)
-print(root)
-sys.path.insert(0, root)
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.join(docs, 'sphinxext'))
-sys.path.insert(0, os.path.join(root, 'binderhub/'))
-print(sys.path)
+sys.path.insert(0, os.path.join(project_root, 'binderhub'))
+sys.path.append(os.path.join(project_root, 'doc/reference'))
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
